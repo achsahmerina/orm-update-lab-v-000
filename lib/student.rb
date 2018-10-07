dd5120e401e57bd44ad0cb15e4fd5d01f5b68e37
Achsah Abraham <achsahmp@gmail.com>
2018-10-07 00:18:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,8 +38,13 @@ class Student
     end
 
     def self.create(name, grade)
-    student = self.new(name, grade)
-    student.save
-    student
+      student = self.new(name, grade)
+      student.save
+      student
+    end
+
+    def self.new_from_db(row)
+      student = self.new(row[1], row[2], row[0])
+      student
     end
 end
