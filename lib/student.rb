fd8d6b65a8dbab305f53626d020987dc0dd5b060
Achsah Abraham <achsahmp@gmail.com>
2018-10-07 00:19:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,4 +47,10 @@ class Student
       student = self.new(row[1], row[2], row[0])
       student
     end
+
+    def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ? LIMIT 1
+    SQL
+  end
 end
